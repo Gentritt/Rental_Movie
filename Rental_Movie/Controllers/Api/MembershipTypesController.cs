c0bccc52cc2086f5ce532128842587e169334110
gentritt <gentrit.selimi@riinvest.net>
2021-03-31 14:15:05 +0200

    show memberships in datatable

---
+++ Rental_Movie/Controllers/Api/MembershipTypesController.cs
@@ -0,0 +1,28 @@
+﻿using Rental_Movie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Rental_Movie.Controllers.Api
+{
+    public class MembershipTypesController : ApiController
+    {
+        private static ApplicationDbContext _context;
+		public MembershipTypesController()
+		{
+            _context = new ApplicationDbContext();
+		}
+        //Get/membershiptypes/
+        public IHttpActionResult GetMemberships()
+		{
+            var memberships=  _context.membershipTypes.ToList();
+            return Ok(memberships);
+		}
+
+
+
+    }
+}

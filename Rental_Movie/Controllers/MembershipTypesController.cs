2323e33ff59d3c806d7bfef5fea2e5462d6eb602
gentritt <gentrit.selimi@riinvest.net>
2021-03-31 14:22:12 +0200

    adding a new membership !

--- Rental_Movie/Controllers/MembershipTypesController.cs
+++ Rental_Movie/Controllers/MembershipTypesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rental_Movie.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,40 @@ namespace Rental_Movie.Controllers
 {
     public class MembershipTypesController : Controller
     {
+        private static ApplicationDbContext _context;
+		public MembershipTypesController()
+		{
+            _context = new ApplicationDbContext();
+		}
         // GET: MembershipTypes
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult New (MembershipType membershipType)
+		{
+            return View("MembershipTypeForm");
+		}
+
+        [HttpPost]
+        public ActionResult Save(MembershipType membershipType)
+		{
+            if (membershipType.Id == 0)
+                _context.membershipTypes.Add(membershipType);
+			else
+			{
+                var membershipindb = _context.membershipTypes.Single(c => c.Id == membershipType.Id);
+
+                membershipindb.Name = membershipType.Name;
+                membershipindb.SignUpFee = membershipType.SignUpFee;
+                membershipindb.DiscountRate = membershipType.DiscountRate;
+                membershipindb.DurationInMonths = membershipType.DurationInMonths;
+			}
+            _context.SaveChanges();
+            return RedirectToAction("Index", "MembershipTypes");
+                
+
+		}
     }
 }
--- Rental_Movie/Models/MembershipType.cs
+++ Rental_Movie/Models/MembershipType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace Rental_Movie.Models
 	public class MembershipType
 	{
 
+		[Required]
 		public string Name { get; set; }
 		public int Id { get; set; }
 		public short SignUpFee { get; set; }

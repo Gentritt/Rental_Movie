9c2cfb4593951e2b2fb47043096d2b646206d413
gentritt <gentrit.selimi@riinvest.net>
2021-04-02 20:33:10 +0200

    Genres Crud Operations addded from API

---
+++ Rental_Movie/Controllers/Api/GenresController.cs
@@ -0,0 +1,54 @@
+﻿using Rental_Movie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Rental_Movie.Controllers.Api
+{
+    public class GenresController : ApiController
+    {
+        private static ApplicationDbContext _context;
+		public GenresController()
+		{
+            _context = new ApplicationDbContext();
+		}
+
+        public IHttpActionResult GetGenres()
+		{
+            var genres = _context.Genres.ToList();
+            return Ok(genres);
+		}
+
+        public IHttpActionResult GetGenre(int id)
+		{
+            var genre = _context.Genres.SingleOrDefault(x => x.Id == id);
+            if (genre == null)
+                return NotFound();
+            return Ok(genre);
+		}
+        [HttpPost]
+        public Genre createGenre(Genre genre)
+		{
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            _context.Genres.Add(genre);
+            _context.SaveChanges();
+            return genre;
+		}
+
+        [HttpDelete]
+        public void DeleteGenre(int id)
+		{
+            var genre = _context.Genres.SingleOrDefault(x => x.Id == id);
+            if (genre == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            _context.Genres.Remove(genre);
+            _context.SaveChanges();
+		}
+
+
+    }
+}
---
+++ Rental_Movie/Controllers/GenresController.cs
@@ -0,0 +1,37 @@
+﻿using Rental_Movie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rental_Movie.Controllers
+{
+    public class GenresController : Controller
+    {
+        private static ApplicationDbContext _context;
+		public GenresController()
+		{
+            _context = new ApplicationDbContext();
+		}
+        // GET: Genres
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult New()
+		{
+            return View("GenreForm");
+		}
+
+        [HttpPost]
+        public ActionResult Save(Genre genre)
+		{
+            if (genre.Id == 0)
+                _context.Genres.Add(genre);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Genres");
+		}
+    }
+}
